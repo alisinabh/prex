25e1b12587337d9e492bf7c745c38e1c988cc622
alisinabh <alisina.bm@gmail.com>
2017-06-17 01:34:45 +0430

    Fix System.cmd calls in mix task

--- lib/mix/tasks/from_blueprint.ex
+++ lib/mix/tasks/from_blueprint.ex
@@ -5,35 +5,46 @@ defmodule Mix.Tasks.Prex.Gen.FromBlueprint do
 
   use Mix.Task
 
-  import Prex.NameHelpers
-
   @shortdoc "Create interface of API based on blueprint json"
 
-  def run([]) do
+  def run([filename, api_name]) do
+    {current_dir, _} = System.cmd "pwd", []
+    current_dir = String.slice current_dir, 0, String.length(current_dir) -1
+
+    full_name =
+      if filename |> String.starts_with?("/") do
+          filename
+        else
+          Path.join current_dir, filename
+      end
+
+    if full_name |> String.downcase |> String.ends_with?(".apib") do
+      {data, _} = System.cmd("drafter", ["-t" ,"ast", "-f", "json", full_name])
+      File.rm "api.json"
+      File.write("api.json", data)
+    else
+      System.cmd("cp", ["#{full_name}", "api.json"])
+    end
+
+    file = Prex.AstFile.parse_file! "api.json"
+    groups = Prex.AstFile.get_resource_groups file
+    host = Prex.AstFile.get_host file
+
+    process_groups groups, api_name, host
+  end
+
+  def run(_) do
     IO.puts "mix prex.gen.from_blueprint [blueprint or json blueprint file] [a name for this api]"
   end
 
-  def run([filename, api_name]) do
-
-    # if filename |> String.downcase |> String.ends_with?(".apib") do
-    #   System.cmd("drafter -t ast -f json #{filename} > api.json", [])
-    # else
-    #   System.cmd("cp #{filename} ~/api.json", [])
-    # end
-
-    file = Prex.AstFile.parse_file! filename
-    groups = Prex.AstFile.get_resource_groups(file)
-    host = Prex.AstFile.get_host(file)
-
-    process_groups(groups, api_name, host)
-  end
-
   defp process_groups([gp | tail], api_name, base_url) do
-     {:ok, filename, code} = Prex.AstFile.generate_module(api_name ,gp, base_url)
+     {:ok, filename, code} = Prex.generate_module(api_name ,gp, base_url)
      # File.open(filename, :write)
-     
+
      File.touch filename
      File.write filename, code
+
+     process_groups(tail, api_name, base_url)
   end
 
   defp process_groups([], _, _), do: :ok
--- lib/prex.ex
+++ lib/prex.ex
@@ -1,3 +1,38 @@
 defmodule Prex do
-  
+  @moduledoc """
+  Prex module is used to generate actions
+  """
+
+  import Prex.NameHelpers
+  import Prex.AstFile
+
+  @doc """
+  Generate elixir modules for a group
+  """
+  @spec generate_module(String.t, Map.t, String.t) :: {:ok, String.t, String.t}
+  def generate_module(api_name, group, base_url) do
+    %{"name" => group_name, "description" => group_description, "resources" => resources} = group
+    actions_code = do_generate_module(base_url, group_name, group_description, resources, [])
+
+    {:ok,
+     "#{normalize_var_name(api_name)}__#{normalize_var_name(group_name)}.ex", # TODO fix folders
+     Prex.Templates.get_module(api_name, group_name, base_url, group_description, actions_code)}
+  end
+
+  defp do_generate_module(base_url, group_name, group_description, [resource | tail], acc) do
+    %{"name" => name, "uriTemplate" => uri, "parameters" => global_params, "actions" => actions} = resource
+
+    get_actions(uri, global_params, actions, "")
+  end
+
+  defp do_generate_module(_group_name, _group_description, [], acc), do: acc
+
+  defp get_actions(uri, global_params, [action | tail], acc) do
+    %{"name" => name, "description" => description, "method" => method, "parameters" => parameters} = action
+    function_string = Prex.Templates.action_template(name, uri, method, global_params ++ parameters, description)
+
+    get_actions(uri, global_params, tail, acc <> function_string)
+  end
+
+  defp get_actions(_uri, _global_params, [], acc), do: acc
 end
