14c6b9b0d6bf5ad76474bdf73145bc55d05edf64
alisinabh <alisina.bm@gmail.com>
2017-06-17 03:12:48 +0430

    Add package to hex

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Prex
+[![Status](https://img.shields.io/badge/status-under%20development-red.svg)]()
 
 Tasks for generating api interfaces in elixir from ApiBlueprint files.
+
+**This project is under development and in experimental stage**
 
 ## Installation
 
--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Prex.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [app: :prex,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     description: description(),
+     package: package(),
+     deps: deps(),
+     name: "Prex",
+     source_url: "https://github.com/alisinabh/prex"]
   end
 
   # Configuration for the OTP application
@@ -15,7 +21,7 @@ defmodule Prex.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :poison]]
+    [extra_applications: [:poison]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +34,25 @@ defmodule Prex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:poison, "~> 3.1"}]
+    [{:poison, "~> 3.1"},
+     {:ex_doc, ">= 0.0.0", only: :dev}]
   end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      name: :prex,
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Alisina Bahadori"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/alisinabh/prex"}
+    ]
+  end
+
+  defp description do
+   """
+   Prex is a REST API client code generator for elixir
+   """
+  end
+
 end
